7c8d34c3ef92a46ad49d5a3f4cc31ec95cbcf6c5
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-03-06 23:37:59 -0500

    Initial Commit

--- src/Anagrama.java
+++ src/Anagrama.java
@@ -1,12 +1,18 @@
 public class Anagrama {
     public static void createAnagrams(String word, String aux){
-        if (word.length() == 0) {
-            System.out.println(aux);
+        if (word.length() == 0) { //Verifica la el caso base, donde no hay mas letras a añadir;
+            System.out.println(aux); //Imprime el resultado
         } else {
-            for (int i = 0; i < word.length(); i++) {
-                createAnagrams(word.substring(0, i) + word.substring(i + 1),aux + word.charAt(i));
+            for (int i = 0; i < word.length(); i++) { //Recorre las letras en el argumento word
+                //Elimina la letra en la posicion i. En el primer llamado, "mora", pasa a ser "ora"
+                String newWord=word.substring(0, i) + word.substring(i + 1);
+
+                //Compienza a concatenar el camino. En el primer llamado, se toma "m"
+                String letter = aux+word.charAt(i);
+
+                //Llamado recursivo
+                createAnagrams(newWord,letter);
             }
         }
     }
-
 }
